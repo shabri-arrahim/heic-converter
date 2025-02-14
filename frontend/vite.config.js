98ab5e83f249b90d500a5c04c1c952b6f8ede1c7
shabri-arrahim <shabriel.bathny@gmail.com>
2025-02-14 11:39:55 +0700

    fix: FE proxy configurations

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://backend:8000',
+        target: 'http://heic-backend:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
